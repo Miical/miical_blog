8a51595ba84b1fc32753e3cef52263a60758c206
Miical <879754743@qq.com>
2022-04-30 09:57:47 +0800

    Update layout & sidebar

--- client/src/components/modules/ArticleList.js
+++ client/src/components/modules/ArticleList.js
@@ -5,7 +5,10 @@ import "./ArticleList.css";
 const ArticleList = () => {
   return (
     <div className="ArticleList-container shadow-md">
-      <p>wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.</p>
+      <div className="ArticleList-titleContainer">
+        <div className="ArticleList-title">Article List</div>
+      </div>
+      <p>wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi.</p>
       <p>wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. </p>
       <p>wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. wangning shi hanpi. </p>
     </div>
--- client/src/components/modules/SideBar.js
+++ client/src/components/modules/SideBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
 import ArticleList from "./ArticleList";
+import "./SideBar.css"
 
 const SideBar= () => {
   return (
-    <div>
+    <div className="SideBar-adapter">
       <ProfileCard />
       <ArticleList />
     </div>
