b03407ffc0efdc5cf417e6ae8e1f01110175b580
Miical <879754743@qq.com>
2022-05-01 10:20:43 +0800

    Add code highlight

--- client/src/components/modules/Article.js
+++ client/src/components/modules/Article.js
@@ -4,7 +4,11 @@ import "./Article.css";
 import ReactMarkdown from "react-markdown";
 import ReactDom from "react-dom";
 import remarkGfm from "remark-gfm";
-import "./github-markdown-css/github-markdown.css"
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "./github-markdown-css/github-markdown.css";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {coy   } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 /**
  * @param article
@@ -26,15 +30,16 @@ const Article = (props) => {
     reader.readAsText(e.target.files[0]);
   };
 
-  let md='# 这是标题\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(Hello World)\` \n\n'+
-  '```const a=2; ```'
+  let md =
+    "# 这是标题\n" +
+    "[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n" +
+    "> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n" +
+    "**这是加粗的文字**\n\n" +
+    "*这是倾斜的文字*`\n\n" +
+    "***这是斜体加粗的文字***\n\n" +
+    "~~这是加删除线的文字~~ \n\n" +
+    "`console.log(Hello World)` \n\n" +
+    "```const a=2; ```";
 
   return (
     <div className="Article-container">
@@ -53,8 +58,27 @@ const Article = (props) => {
         <ReactMarkdown
           children={markdown}
           escapeHtml={false}
-          remarkPlugins={[remarkGfm]}
+          remarkPlugins={[remarkGfm, remarkMath]}
+          rehypePlugins={[rehypeKatex]}
           className="markdown-body"
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, "")}
+                  style={coy}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
         />
       </div>
     </div>
