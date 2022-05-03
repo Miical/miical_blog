7c5c8db0a9627effb6bfe39ed8df58f35316a053
Miical <879754743@qq.com>
2022-05-03 15:51:08 +0800

    Fix sidebar bug

--- client/src/components/modules/SideBar.js
+++ client/src/components/modules/SideBar.js
@@ -3,27 +3,26 @@ import ProfileCard from "./ProfileCard";
 import ArticleList from "./ArticleList";
 import UploadArticle from "./UploadArticle";
 import UploadImage from "./UploadImage";
-import "./SideBar.css"
+import "./SideBar.css";
 
-const SideBar= () => {
-  (function(){    
-      let oDiv=document.getElementById("SideBar-adapter");    
-      let iE6=window.ActiveXObject&&!window.XMLHttpRequest;    
-      if(!iE6){    
-          window.onscroll=function()    
-          {    
-            let screenH = document.documentElement.scrollTop + window.innerHeight;
-            let divH = oDiv.clientHeight;
-            if (screenH > divH) {
-              oDiv.style.position = "sticky";
-              oDiv.style.top = window.innerHeight-divH +"px";
-            } else {
-              oDiv.style.position = "relative";
-              oDiv.style.top = 0;
-            }
-          };    
-      }    
-  })();   
+const SideBar = () => {
+  (function () {
+    let iE6 = window.ActiveXObject && !window.XMLHttpRequest;
+    if (!iE6) {
+      window.onscroll = function () {
+        let oDiv = document.getElementById("SideBar-adapter");
+        let screenH = document.documentElement.scrollTop + window.innerHeight;
+        let divH = oDiv.clientHeight;
+        if (screenH > divH) {
+          oDiv.style.position = "sticky";
+          oDiv.style.top = window.innerHeight - divH + "px";
+        } else {
+          oDiv.style.position = "relative";
+          oDiv.style.top = 0;
+        }
+      };
+    }
+  })();
   return (
     <div className="SideBar-adapter" id="SideBar-adapter">
       <ProfileCard />
