201aab86979c5876f9e1c851b7cf0df9ddab83fb
Miical <879754743@qq.com>
2022-05-22 23:04:57 +0800

    Update ProfileCard

--- client/src/components/modules/ProfileCard.js
+++ client/src/components/modules/ProfileCard.js
@@ -1,18 +1,30 @@
 import React from "react";
-
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
+import bgImg from "../../public/img/bg.jpg"
 import "./ProfileCard.css";
 
 const ProfileCard = () => {
   return (
-    <div className="ProfileCard-container shadow-md">
-      <div className="ProfileCard-avatar" />
-      <div className="text-center">
-        <span className="ProfileCard-title">Miical</span>
-        <span className="ProfileCard-subtitle"> 's blog</span>
-      </div>
-      <hr className="ProfileCard-line"/>
-
-    </div>
+<Card style={{ width: '17rem' }}>
+  <Card.Img variant="top" src={bgImg}/>
+  <Card.Body>
+    <Card.Title>Miical's blog</Card.Title>
+    <Card.Text>
+      Welcome to my blog! <br/>
+      Here I will document my experience of studying computer science and technology<br/>
+      Progress Together!
+    </Card.Text>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>University: China University of Geosciences (Beijing)</ListGroupItem>
+    <ListGroupItem>Email: jason.ljcer@gmail.com</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="https://github.com/Miical">Github</Card.Link>
+  </Card.Body>
+</Card>
   );
 };
 
